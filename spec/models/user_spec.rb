c9eaf251f2cb07371ca1d49b735ef68e3cc6ddbf
Alexandre Mondaini Calvão <apotema@gmail.com>
2023-11-08 19:04:26 -0300

    Missing specs for user clerk

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,21 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it { is_expected.to have_one(:member) }
+
+  describe '#clerk?' do
+    let(:clerk) { create(:user, role: 'clerk') }
+    let(:non_clerk) { create(:user, role: 'member') }
+
+    context 'when the user is a clerk' do
+      it 'returns true' do
+        expect(clerk.clerk?).to be true
+      end
+    end
+
+    context 'when the user is not a clerk' do
+      it 'returns false' do
+        expect(non_clerk.clerk?).to be false
+      end
+    end
+  end
 end
